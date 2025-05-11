7f27500762231a03e728bd60fe0d411f1a7ac2bf
eevgeniiiiia <ev.sergeevna23@mail.ru>
2025-05-11 13:51:24 +0300

    Update 03.js

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -13,9 +13,9 @@ if(score === 0 && score <= 49 )
 {
     grade = 'F';
 } else if (score >= 50 && score <= 69 ) {
-    grade = 'C';
-} else if (score >= 70 && score <= 79) {
     grade = 'D';
+} else if (score >= 70 && score <= 79) {
+    grade = 'C';
 } else if (score >= 80 && score <= 89) {
     grade = 'B';
 } else if (score >= 90 && score <= 100) {
@@ -23,5 +23,4 @@ if(score === 0 && score <= 49 )
 } else {
     grade = "Error"
 }
-
 
